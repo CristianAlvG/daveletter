b5187acc7902f765e9b5bda5708c5ca8763afd34
CristianAlvG <cristian98x@hotmail.com>
2024-11-14 11:21:10 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# Setting page layout to wide
+st.set_page_config(layout="wide", page_title="Dave's Letter", page_icon="images/mario.png")
+
+col3, col4 = st.columns([2,4])
+
+with col3:
+    st.image("images/mario.png", caption="Toad")
+
+with col4:
+    st.markdown("<h1 style='text-align: center;'>¿Como juzgar el éxito?</h1>", unsafe_allow_html=True)
+    with open("letter.txt", 'r', encoding="utf-8") as file:
+        carta = file.read()
+
+    st.warning(carta)
